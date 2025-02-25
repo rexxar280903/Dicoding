4ddcd8bb3d30098bd17edcf8f0c777f4c0fa30c0
rexxar280903 <rexxar280903@gmail.com>
2025-02-25 14:20:23 +0700

    contoh

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Aplikasi Web dengan Streamlit")
+st.header("Ini adalah header")
+st.subheader("Ini adalah subheader")
+st.write("Halo! Ini adalah aplikasi pertama menggunakan Streamlit.")
